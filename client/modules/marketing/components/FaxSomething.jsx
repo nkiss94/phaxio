0085253890a180f883186d8cebd1f34eb546acde
Adam Kovalcik <akovalcik@wealthsimple.com>
2017-06-20 09:35:30 -0400

    First commit for development branch (Phase 1)

--- client/modules/marketing/components/FaxSomething.jsx
+++ client/modules/marketing/components/FaxSomething.jsx
@@ -6,103 +6,160 @@ import Autocomplete from 'react-autocomplete';
 import ErrorNotification from './ErrorNotification';
 import SuccessNotification from './SuccessNotification';
 
-class FaxSomething extends React.Component {
+export default class FaxSomething extends React.Component {
   constructor(props) {
     super(props);
     this.sendFax=this.sendFax.bind(this);
+    this.checkInput=this.checkInput.bind(this);
     this.state = {
-       institution : 'Shareowner',
-       err: null,
-       success: null
-  }
+     institution : null,
+     err: null,
+     success: null,
+     number: null
+   }
+ }
+
+ checkInput(value){
+  this.setState({institution:value});    
+  this.setState({number: value})
+  console.log(this.state.number);  
 }
 
+//meteor --settings settings.json
 sendFax(){
-    const url = this.refs.url.value
-    const institution = this.state.institution
-    for (var i=0; i < faxable_institutions.length; i++) {
-      const _this = this;
-        if (faxable_institutions[i].name === institution) {
-            let institution = faxable_institutions[i].name
-            let number = faxable_institutions[i].number
-            Meteor.call('sendPhaxio', institution, number, function(err, resp){
+  const url = this.refs.url.value
+  const _this = this;
+  console.log(this.state.number);
+            Meteor.call('sendPhaxio', this.state.number, url,  function(err, resp){
               if(err){
                 _this.setState({err: err})
               }
                 _this.setState({sucess: resp})
-            })
-        }
-    }
-}
+              })
+            }
 
-  render() {
-    return (
-      <div className="ws-layout-container ws-account-transfer">
-        <div className="footer-padding">
-            <div className="card-container">
-                <div className="row-md header">
-                    <div className="col-6">
-                        <div className="title">
-                            Funding
-                        </div>
-                    </div>
-                    <div className="col-6">
-                        <div className="buttons">
-                            <div className="button-new secondary-action">Logout</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row-md">
-                    <div className="col-12">
-                        <div className="card centered-question">
-                            <div className="section content size-xs">
-                                Enter your pdf and choose an institution
-                            </div>
-                           
-                            <div>Hi!!!</div>
-                            <input placeholder="enter url" ref="url" className="form-control" />
-                            <Autocomplete
-                              value={this.state.institution}
-                              items={faxable_institutions}
-                              getItemValue={(item) => item.name}
-                              onChange={(event, value) => this.setState({ institution: value })}
-                              onSelect={value => this.setState({ institution: value })}
-                              renderItem={(item, isHighlighted) => (
-                                <div
-                                  style={isHighlighted ? styles.highlightedItem : styles.item}
-                                  key={item.name}
-                                >{item.name}</div>
+            render() {
+              return ( 
+                <div className="container">       
+                  <hr></hr>
+                  <div className="row center">
+                    <div className="pageTitle">FaxSimple</div>
+                  </div>  
+                  <hr></hr>
+                  <div className="center row dialogue">What's your PDF URL?</div>
+                  <div className="row ">
+                      <div className = "col-lg-4 col-md-4 col-sm-4 col-xs-4"></div>
+                      <div className = "centerME col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                          
+                          <input
+                          placeholder="url"
+                          ref="url"
+                          className="url form-control col-lg-8 col-md-8 col-sm-6 col-xs-6"
+                          style={{
+                            boxShadow:'none',
+                            borderRadius:'4px'
+
+
+                          }}
+                          />
+                          <button
+                              className= "send btn btn-default col-lg-4 col-md-4 col-sm-6 col-xs-6"
+                                  style={{
+                                    
+                                    boxShadow:'none',
+                                    borderRadius:'4px',
+                                  
+                                    marginRight:'0'
+                                  }}>
+                                  Add
+                          </button>
+                      </div>
+                      <div className = "col-lg-4 col-md-4 col-sm-4 col-xs-4"></div>
+                  </div>
+                  <div className="center row dialogue">What's the fax number?</div>
+                  <div className="row"> 
+                      <div className = "col-lg-4 col-md-4 col-sm-4 col-xs-4"></div>
+                      <div className="center col-lg-4 col-md-4 col-sm-4 col-xs-4"> 
+                        <div className="center col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                          <button 
+                              className= "send btn btn-default"
+                              style={{
+                                
+                                boxShadow:'none',
+                                borderRadius:'4px',
+                              
+                                marginRight:'0'
+                              }}
+                              onClick={this.sendFax}>send fax
+                          </button>
+                        </div> 
+                        <div className="center col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                          <Autocomplete
+                          inputProps={{
+
+                            className: 'form-control',
+                            placeholder: 'institution fax #',
+                            style: {
+                              
+                              boxShadow:'none',
+                              background: 'rgba(255, 255, 255, 0.9)',
+                              borderBottomRightRadius:'0px',
+                              borderBottomLeftRadius:'0px',
+                              width:'200px'
+
+                            }
+                          }}
+                          menuStyle = {{
+
+                            borderRadius: '4px',
+                            borderTopRightRadius:'0px',
+                            borderTopLeftRadius:'0px',
+                            background: '#d1d1e0',
+                          }}
+                          value={this.state.institution}
+                          items={faxable_institutions}
+                          getItemValue={(item) => item.number}
+                          onChange={(event, value) => this.checkInput(value)}
+                          onSelect={
+                            value => this.checkInput(value)}
+                            renderItem={(item, isHighlighted) => (
+                              <div
+                              style={isHighlighted ? styles.highlightedItem : styles.item}
+                              key={item.name}
+                              >
+                              {item.name}
+                              </div>
                               )}
-                            />
-                            <button onClick={this.sendFax}>send fax</button>
-                            {this.state.err ? <ErrorNotification /> : null}
-                            {this.state.success ? <SuccessNotification /> : null}
+                            >
+                            </Autocomplete>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-    )
-  }
-}
+                        
 
-export default FaxSomething;
+                      </div> 
+                      <div className = "col-lg-4 col-md-4 col-sm-4 col-xs-4"></div> 
 
-export let styles = {
-  item: {
-    padding: '2px 6px',
-    cursor: 'default'
-  },
+                  </div>
+                  {this.state.err ? <ErrorNotification /> : null}
+                  {this.state.success ? <SuccessNotification /> : null}
+                  </div>    
+                  )
+            }
+          }
 
-  highlightedItem: {
-    color: 'white',
-    background: 'hsl(200, 50%, 50%)',
-    padding: '2px 6px',
-    cursor: 'default'
-  },
+          export let styles = {
+            item: {
+              padding: '2px 6px',
+              cursor: 'default'
+            },
 
-  menu: {
-    border: 'solid 1px #ccc'
-  }
-}
+            highlightedItem: {
+              color: 'white',
+              background: 'hsl(200, 50%, 50%)',
+              padding: '2px 6px',
+              cursor: 'default'
+            },
+
+            menu: {
+              border: 'solid 1px #ccc'
+            }
+          }
