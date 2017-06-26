e7faf9e6accb47bf54957b295c4f2aa493749a54
Nick Kiss <Kiss@Admins-MacBook-Air.local>
2017-06-26 13:21:20 -0400

    added new institutions to config file

--- config/faxable_institutions.js
+++ config/faxable_institutions.js
@@ -1,11 +1,29 @@
 let faxable_institutions = [
+    { name: 'BMO Branch', 
+      number: '+15148771466'
+    },
+    { name: 'CIBC Branch', 
+      number: '+18002859620'
+    },
+    { name: 'Fundex', 
+      number: '+19053051698'
+    },
+    { name: 'PC Financial', 
+      number: '+18884366777'
+    },
     { name: 'Shareowner', 
       number: '+14165950400'
     },
     {
-      name: 'TD',
+      name: 'Tangerine',
+      number: '+18774647797'
+    },
+    {
+      name: 'TD Branch',
       number: '+14161234567'
     }
+  
+    
 ]
 
 export default faxable_institutions;
