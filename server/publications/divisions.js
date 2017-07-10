ba834ff3111c0d87ad13fa7c809971b9cde425ab
Tim <Tim@Tims-MacBook-Air.local>
2017-07-10 15:50:06 -0400

    pub sub for divisions

---
+++ server/publications/divisions.js
@@ -0,0 +1,10 @@
+import {Divisions} from '/lib/collections';
+import {Meteor} from 'meteor/meteor';
+import {check} from 'meteor/check';
+
+export default function () { 
+	
+	Meteor.publish('divisions.list', function () {
+		return Divisions.find();
+	});
+}
