471a946440e225349f400206e1542c66363e45e4
Adam Kovalcik <akovalcik@wealthsimple.com>
2017-07-19 09:47:25 -0400

    added files

--- server/methods/phaxio.js
+++ server/methods/phaxio.js
@@ -1,25 +1,15 @@
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { HTTP } from 'meteor/http'; 
 import { Session } from 'meteor/session';
+ 
 
 S3.config = {
   key: Meteor.settings.private.AWS.AWS_ACCESS_KEY_ID,
   secret: Meteor.settings.private.AWS.AWS_SECRET_ACCESS_KEY,
   bucket: 'faxsimpleupload'
 };
-=======
-import { HTTP } from 'meteor/http' ;
-import { Session } from 'meteor/session';
-import {Divisions} from '/lib/collections';
 
->>>>>>> 0e97943be6c8de56ccc1719916506748bee60add
-=======
-import { HTTP } from 'meteor/http' ;
-import { Session } from 'meteor/session';
-import { ObjectID, toHexString } from 'mongodb';
 var fs = require('fs');
 
 var AWS = require('aws-sdk');
@@ -31,7 +21,7 @@ AWS.config.update({
 });
 
 var s3 = new AWS.S3(); 
->>>>>>> e6d333ba630737444a997fc3f1948642e3b0e46e
+
 
 export default function () {
     Meteor.methods({
