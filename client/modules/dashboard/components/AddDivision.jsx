df07cd028356065910da64963ad2f1b9d8989914
Nick Kiss <Kiss@Admins-MacBook-Air-5.local>
2017-08-01 14:16:25 -0400

    added dropdown to add division component for hard validation rules

--- client/modules/dashboard/components/AddDivision.jsx
+++ client/modules/dashboard/components/AddDivision.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from 'react-router';
 import {browserHistory} from 'react-router';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
+import Dropdown from 'react-dropdown';
+import ValidationDropdown from './ValidationDropdown';
+
 
 export default class AddDivision extends React.Component {
 	constructor(props){
 		super(props);
 		this.createDivision=this.createDivision.bind(this);
+		this._onSelect = this._onSelect.bind(this)
 		this.state={
 			"Parent": null,
 		    "Division": null,
@@ -17,7 +21,8 @@ export default class AddDivision extends React.Component {
 		    "Fax Number": null,
 		    "Follow-Up Call Number": null,
 		    "Follow-Up Email": null,
-		    "Other Intel": []
+		    "Other Intel": [],
+		    selected: 'none'
 		}
 	}
 
@@ -35,33 +40,49 @@ export default class AddDivision extends React.Component {
 		Meteor.call('insert.division', parent, division, hard_validation, soft_validation, method, aton_cuid, fax_number, call_number, email, other_intel)
 	}
 
-render(){
+	_onSelect (option) {
+	    console.log('You selected ', option.label)
+	    this.setState({selected: option.value})
+	    console.log(option)
+  	}
+
+	render(){
+	const options = [
+		{ value: 0, label: 'none' },
+		{ value: 1, label: 'transfer type' },
+		{ value: 2, label: 'account type' },
+		{ value: 3, label: 'account number length' },
+		{ value: 4, label: 'account number contents' },
+		{ value: 5, label: 'starts with' }
+	]
+	const defaultOption = options[0]
 	return(
-	<div className="container">	
-      	<div className="cards jumbotron centerME"> 
-			<input className="divisionInput" placeholder="Parent" ref="parent" />
-			<br />
-			<input placeholder="Division" ref="division"  />
-			<br />
-			<input placeholder="Hard Validation" ref="hard_validation" />
-			<br />
-			<input placeholder="Soft Validation" ref="soft_validation" />
-			<br />
-			<input placeholder="Method" ref="method" />
-			<br />
-			<input placeholder="ATON CUID" ref="aton_cuid" />
-			<br />
-			<input placeholder="Fax Number" ref="fax_number" />
-			<br />
-			<input placeholder="Follow-up Call Number" ref="call_number" />
-			<br />
-			<input placeholder="Follow-up Email" ref="email"/>
-			<br />
-			<input placeholder="Other Intel" ref="other_intel"/>
-			<br />
-			<button onClick={() => this.createDivision()}>create division</button>
-    	</div>
-    </div>
+		<div className="container">	
+	      	<div className="cards jumbotron centerME"> 
+				<input className="divisionInput" placeholder="Parent" ref="parent" />
+				<br />
+				<input placeholder="Division" ref="division"  />
+				<br />
+				<Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+				{this.state.selected != 'none' ? <ValidationDropdown /> : null}
+				<br />
+				<input placeholder="Soft Validation" ref="soft_validation" />
+				<br />
+				<input placeholder="Method" ref="method" />
+				<br />
+				<input placeholder="ATON CUID" ref="aton_cuid" />
+				<br />
+				<input placeholder="Fax Number" ref="fax_number" />
+				<br />
+				<input placeholder="Follow-up Call Number" ref="call_number" />
+				<br />
+				<input placeholder="Follow-up Email" ref="email"/>
+				<br />
+				<input placeholder="Other Intel" ref="other_intel"/>
+				<br />
+				<button onClick={() => this.createDivision()}>create division</button>
+	    	</div>
+	    </div>
 		)
 	}
 }
---
+++ client/modules/dashboard/components/ValidationDropdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {browserHistory} from 'react-router';
+
+class ValidationDropdown extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div>text</div>          
+            </div>
+    )
+  }
+}
+
+export default ValidationDropdown;
