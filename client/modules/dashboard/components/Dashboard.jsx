51d98665bc117974ee1ec72ba46d54dd86537307
Adam Kovalcik <akovalcik@wealthsimple.com>
2017-08-18 21:48:52 -0400

    added support for adding and editing divisions

--- client/modules/dashboard/components/Dashboard.jsx
+++ client/modules/dashboard/components/Dashboard.jsx
@@ -5,11 +5,13 @@ import FaxCont from '../containers/FaxCont';
 import CheckDigits from './CheckDigits';
 import DivisionsCont from '../containers/DivisionsCont';
 import AddDivisionCont from '../containers/AddDivisionCont';
+import EditDivisionCont from '../containers/EditDivisionCont';
 
 class Dashboard extends React.Component {
     constructor(props) {
     super(props);
     this.selectTab = this.selectTab.bind(this);
+    this.selectEditTab = this.selectEditTab.bind(this);
     this.state = {
         active_tab: 'fax'
     }
@@ -17,6 +19,52 @@ class Dashboard extends React.Component {
 
   selectTab(value){
     this.setState({active_tab: value})
+  }
+  selectEditTab(
+     active_tab,
+     id,
+     institution,
+     cashOnly,
+     division,
+     onlyRegisteredAccounts,
+     digitsOnly,
+     Alphanumeric,
+     accountNumPattern,
+     AccountNumLength,
+     startsWith, 
+     soft_validation, 
+     method,
+     fax_aton_number, 
+     call_number, 
+     email, 
+     other_intel,
+     address,
+     city,
+     province,
+     postalCode){
+    this.setState({
+        active_tab,
+        id,
+        institution,
+        cashOnly,
+        division, 
+        onlyRegisteredAccounts,
+        digitsOnly,
+        Alphanumeric,
+        accountNumPattern,
+        AccountNumLength,
+        startsWith, 
+        soft_validation, 
+        method,fax_aton_number, 
+        call_number, 
+        email, 
+        other_intel,
+        address,
+        city,
+        province,
+        postalCode
+    });
+    console.log(this.state);
   }
 
     render() {
@@ -26,8 +74,32 @@ class Dashboard extends React.Component {
                 <div>  
                     {this.state.active_tab === "fax" ? <FaxCont /> : null}
                     {this.state.active_tab === 'check_digits' ? <CheckDigits /> : null}
-                    {this.state.active_tab === 'divisions' ? <DivisionsCont /> : null} 
-                    {this.state.active_tab === 'addDivision' ? <AddDivisionCont /> : null}                   
+                    {this.state.active_tab === 'divisions' ? <DivisionsCont 
+                        selectEditTab={this.selectEditTab}
+                    /> : null} 
+                    {this.state.active_tab === 'addDivision' ? <AddDivisionCont /> : null}   
+                    {this.state.active_tab === 'editDivision' ? <EditDivisionCont
+                        id={this.state.id}
+                        institution={this.state.institution}
+                        cashOnly={this.state.cashOnly}
+                        division={this.state.division}
+                        onlyRegisteredAccounts={this.state.onlyRegisteredAccounts}
+                        digitsOnly={this.state.digitsOnly}
+                        Alphanumeric={this.state.Alphanumeric}
+                        accountNumPattern={this.state.accountNumPattern}
+                        AccountNumLength={this.state.AccountNumLength}
+                        startsWith={this.state.startsWith}
+                        soft_validation={this.state.soft_validation}
+                        method={this.state.method}
+                        fax_aton_number={this.state.fax_aton_number}
+                        call_number={this.state.call_number}
+                        email={this.state.email}
+                        other_intel={this.state.other_intel}
+                        address={this.state.address}
+                        city={this.state.city}
+                        province={this.state.province}
+                        postalCode={this.state.postalCode}
+                    />: null}                
                 </div>       
             </div>
     )
--- client/modules/dashboard/components/Divisions.jsx
+++ client/modules/dashboard/components/Divisions.jsx
@@ -16,6 +16,7 @@ export default class Divisions extends React.Component {
 		this.loadDivisions=this.loadDivisions.bind(this);
 		this.loadParents=this.loadParents.bind(this);
 		this.findDivisions=this.findDivisions.bind(this);
+		this.editDivision = this.editDivision.bind(this);
 		this.state={
 			value:null,
 			institution:null,
@@ -23,13 +24,19 @@ export default class Divisions extends React.Component {
 			division:null,
 			default:null,
 			allDivisions:[],
-			hardValidation:"hardValidation",
-			softValidation:"softValidation",
-			sendMethod:"sendMethod",
-			faxATON:"faxATON",
-			followUpNum:"followUpNum",
-			followUpEmail:"followUpEmail",
-			otherIntel:"otherIntel"
+			inCashOnly:" ",
+			onlyRegisteredAccounts:" ",
+			accountNumPattern:" ",
+			AccountNumLength:" ",
+			Alphanumeric:" ",
+			digitsOnly:" ",
+			startsWith:" ",
+			softValidation:" ",
+			sendMethod:" ",
+			faxATON:" ",
+			followUpNum:" ",
+			followUpEmail:" ",
+			otherIntel:" "
 		}
 	}
  checkInputInstitution(item){
@@ -59,29 +66,52 @@ saveDivision(divToFind){
 	var index;
 	var length = this.props.divisions.length;
 	for(var i = 0;i<length;i++){
-	 	if(this.props.divisions[i].Division==divToFind){
+	 	if(this.props.divisions[i].name==divToFind){
 	 		index=i;
 	 		break;
 	 	}
 	}
 
 	this.setState({
-		hardValidation:this.props.divisions[index].HardValidation,
-		softValidation:this.props.divisions[index].SoftValidation,
+		id:this.props.divisions[index].id,
+		inCashOnly:this.props.divisions[index].transfer_cash_only,
+		onlyRegisteredAccounts:this.props.divisions[index].registered_account_types_only,
+		accountNumPattern:this.props.divisions[index].account_number_pattern,
+		AccountNumLength:this.props.divisions[index].account_number_lengths,
+		Alphanumeric:this.props.divisions[index].account_number_alphanumeric,
+		digitsOnly:this.props.divisions[index].account_number_digits_only,
+		startsWith:this.props.divisions[index].account_number_starts_with,
+		softValidation:this.props.divisions[index].hint,
 		sendMethod:this.props.divisions[index].Method,
-		faxATON:this.props.divisions[index].FaxNumber + ' ' + this.props.divisions[index].ATONCUID,
-		followUpNum:this.props.divisions[index].FollowUPCallNumber,
+		faxATON:this.props.divisions[index].FaxNumber + ' ' + this.props.divisions[index].cuid,
+		followUpNum:this.props.divisions[index].FollowUpCallNumber,
 		followUpEmail:this.props.divisions[index].FollowUpEmail,
 		otherIntel:this.props.divisions[index].OtherIntel
-	})
+	});
+	if(this.props.divisions[index].address!=null){
+		this.setState({
+			address:this.props.divisions[index].address,
+			city:this.props.divisions[index].city,
+			province:this.props.divisions[index].province,
+			postalCode:this.props.divisions[index].postalCode
+		})
+	}
+	else{
+		this.setState({
+			address:"",
+			city:"",
+			province:"",
+			postalCode:""
+		})
+	}
 	document.getElementById("divisionInfo").style.display = 'block';
 }
 loadParents(){
 	var newParents = [];  
 	 var length = this.props.divisions.length;
 	 for(var i = 0;i<length;i++){
-	 	if(!newParents.includes(this.props.divisions[i].Parent)){
-	 		newParents.push(this.props.divisions[i].Parent);  
+	 	if(!newParents.includes(this.props.divisions[i].parent_institution)){
+	 		newParents.push(this.props.divisions[i].parent_institution);  
 	 	}
 	 	
 	 }
@@ -91,7 +121,7 @@ loadDivisions(){
 	 var newDivisions = [];  
 	 var length = this.props.divisions.length;
 	 for(var i = 0;i<length;i++){
-	 	newDivisions.push(this.props.divisions[i].Division);   
+	 	newDivisions.push(this.props.divisions[i].name);   
 	 }
 	 this.setState({allDivisions:newDivisions});
 	 this.loadParents();
@@ -104,15 +134,44 @@ findDivisions(Parent){
 	 var newDivisions = [];  
 	 var length = this.props.divisions.length;
 	 for(var i = 0;i<length;i++){
-	 	if(this.props.divisions[i].Parent==Parent){
-	 	newDivisions.push(this.props.divisions[i].Division);   
+	 	if(this.props.divisions[i].parent_institution==Parent){
+	 	newDivisions.push(this.props.divisions[i].name);   
 	 	}	
 	 }
 	 this.setState({allDivisions:newDivisions});
 	 this.state.institution = Parent;
 }
+editDivision(){
+	//console.log("I made it");
+	//console.log(this.state);
+	var _this = this;
+	console.log(_this);
+	console.log(_this.state.softValidation);
+	this.props.selectEditTab(
+		'editDivision', 
+		_this.state.id,
+		_this.state.institution,
+		_this.state.inCashOnly,
+		_this.state.division, 
+		_this.state.onlyRegisteredAccounts,
+		_this.state.digitsOnly,
+		_this.state.Alphanumeric,
+		_this.state.accountNumPattern,
+		_this.state.AccountNumLength,
+		_this.state.startsWith, 
+		_this.state.softValidation, 
+		_this.state.sendMethod,
+		_this.state.faxATON, 
+		_this.state.followUpNum, 
+		_this.state.followUpEmail, 
+		_this.state.otherIntel,
+		_this.state.address,
+		_this.state.city,
+		_this.state.province,
+		_this.state.postalCode
+	);
+}
 
-//<button onClick={ () => this.loadDivisions()}>load</button>
 render(){
 	 
 	return(
@@ -182,24 +241,74 @@ render(){
 					</div>
 					<div className="col-lg-4 col-md-0 col-sm-0 col-xs-0">
 						<div className="dialogue">{this.state.division}</div>
+						<button
+				            className= "foc center glyphicon glyphicon-pencil send btn btn-default"
+				            style={{
+				              boxShadow:'none',
+				              borderWidth:'0',
+				              borderRadius:'0',
+				            }}
+				            onClick={this.editDivision}>
+				        </button>
 					</div>
 					<div className="col-lg-2"></div>
       		</div>
       	</div>
       	<div id="divisionInfo" className="jumbotron centerME" style={{display:'none'}}>
-
+      				{this.state.softValidation? (
 		      		<div className="row center">
-							<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+							<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 								<div className="dialogueMed inActive">Soft Validation:</div>
 								<div className="dialogueMed">{this.state.softValidation}</div>
 							</div>
-							<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-								<div className="dialogueMed inActive">Hard Validation:</div>
-								<div className="dialogueMed">{this.state.hardValidation}</div>
-							</div>
 		      		</div>
-	      			<hr></hr>
-					<div className="row center" style={{marginTop:'6%'}}>
+		      		):(<div/>)}
+	      			
+	      			<div className="row center" style={{marginTop:'2%'}}>
+							{this.state.inCashOnly=="TRUE" ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+										<div className="dialogueMed inActive">In-Cash only:</div>
+										<div className="dialogueMed">{this.state.inCashOnly}</div>									
+								</div>
+							):(<div/>)}
+							{this.state.onlyRegisteredAccounts=="TRUE"  ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Only registered accounts:</div>
+									<div className="dialogueMed">{this.state.onlyRegisteredAccounts}</div>
+								</div>
+							):(<div/>)}
+							{this.state.accountNumPattern ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Account # pattern:</div>
+									<div className="dialogueMed">{this.state.accountNumPattern}</div>
+								</div>
+							):(<div/>)}
+							{this.state.AccountNumLength ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Account # length:</div>
+									<div className="dialogueMed">{this.state.AccountNumLength}</div>
+								</div>
+							):(<div/>)}
+							{this.state.Alphanumeric=="TRUE" ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Alphanumeric:</div>
+									<div className="dialogueMed">{this.state.Alphanumeric}</div>
+								</div>
+							):(<div/>)}
+							{this.state.digitsOnly=="TRUE" ? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Digits only:</div>
+									<div className="dialogueMed">{this.state.digitsOnly}</div>
+								</div>
+							):(<div/>)}
+							{this.state.startsWith? (
+								<div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+									<div className="dialogueMed inActive">Starts with:</div>
+									<div className="dialogueMed">{this.state.startsWith}</div>
+								</div>
+							):(<div/>)}
+					</div>
+					<div className="row center">
 						<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 							<div className="dialogueMed inActive">Send Method:</div>
 							<div className="dialogueMed ">{this.state.sendMethod}</div>
@@ -209,23 +318,46 @@ render(){
 							<div className="dialogueMed">{this.state.faxATON}</div>
 						</div>
 					</div>
-					<hr></hr>
-					<div className="row center" style={{marginTop:'6%'}}>		
-						<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-							<div className="dialogueMed inActive">Follow-Up Phone Number:</div>
-							<div className="dialogueMed">{this.state.followUpNum}</div>
-						</div>
-						<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-							<div className="dialogueMed inActive">Follow-Up Email:</div>
-							<div className="dialogueMed">{this.state.followUpEmail}</div>
-						</div>
+					{(this.state.followUpNum||this.state.followUpEmail)? (
+						<hr></hr>
+					):(<div/>)}
+					<div className="row center">
+						{this.state.followUpNum? (		
+							<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+								<div className="dialogueMed inActive">Follow-Up Phone Number:</div>
+								<div className="dialogueMed">{this.state.followUpNum}</div>
+							</div>
+						):(<div/>)}
+						{this.state.followUpEmail? (	
+							<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+								<div className="dialogueMed inActive">Follow-Up Email:</div>
+								<div className="dialogueMed">{this.state.followUpEmail}</div>
+							</div>
+						):(<div/>)}
 					</div>
-					<hr></hr>
-					<div className="row center" style={{marginTop:'6%'}}>	
-						<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-							<div className="dialogueMed inActive">Other Intel:</div><div className="dialogueMed">{this.state.otherIntel}</div>
+					{this.state.address? (
+						<hr></hr>
+					):(<div/>)}
+					{this.state.address? (	
+						<div className="row center">	
+							<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<div className="dialogueMed inActive">Address:</div>
+								<div className="dialogueMed">
+									{this.state.address + ", " + this.state.city + ", " + this.state.province + ", " + this.state.postalCode}
+								</div>
+							</div>
 						</div>
-					</div>
+					):(<div/>)}
+					{this.state.otherIntel? (	
+						<hr></hr>
+					):(<div/>)}
+					{this.state.otherIntel? (	
+						<div className="row center">	
+							<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<div className="dialogueMed inActive">Other Intel:</div><div className="dialogueMed">{this.state.otherIntel}</div>
+							</div>
+						</div>
+					):(<div/>)}			
       	</div>
     </div>
 
@@ -243,7 +375,6 @@ export function renderMenuItem (){
 }
 export let styles = {
   item: {
-   
     zIndex:'1',
     padding: '2px 6px',
     cursor: 'default'
